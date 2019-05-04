eaf95d612783a47f0bdadf16a5585a616e837132
Andrew Chuang <andrewjschuang@gmail.com>
2019-05-04 00:22:19 -0300

    code readability refactor

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
-let canvas, 
-		c, 
-		invaders, 
-		w, 
-		h, 
-		dt, 
-		player, 
-		lives,
-		lastUpdate,
-		leftBtn,
-		rightBtn,
-		fireBtn,
-		div,
-		generation;
+let canvas,
+	c,
+	invaders,
+	w,
+	h,
+	dt,
+	player,
+	lives,
+	lastUpdate,
+	leftBtn,
+	rightBtn,
+	fireBtn,
+	div,
+	generation;
 
 canvas = document.createElement('canvas');
 canvas.width = w = 240;
 canvas.height = h = 480;
-c = canvas.getContext('2d',{alpha: false});
+c = canvas.getContext('2d', { alpha: false });
 if (window.devicePixelRatio > 1) {
 	c.canvas.width = c.canvas.width * window.devicePixelRatio;
 	c.canvas.height = c.canvas.height * window.devicePixelRatio;
-	c.canvas.style.width = w+'px';
-	c.canvas.style.height = h+'px';
+	c.canvas.style.width = w + 'px';
+	c.canvas.style.height = h + 'px';
 	c.scale(window.devicePixelRatio, window.devicePixelRatio);
 }
 leftBtn = document.createElement('button');
@@ -35,187 +35,193 @@ div.appendChild(leftBtn);
 div.appendChild(fireBtn)
 div.appendChild(rightBtn);
 
-function init(){
+function init() {
 	lives = 0;
 	generation = 1;
 	dt = 0;
 	lastUpdate = Date.now();
 	canvas.style.border = "solid";
-	document.body.appendChild( canvas );
-	document.body.appendChild( div );
+	document.body.appendChild(canvas);
+	document.body.appendChild(div);
 	invaders = new Genetics();
 	invaders.createPopulation();
-	player = new Player( w/4/2, h/4-4 );
+	player = new Player(w / 4 / 2, h / 4 - 4);
 	update();
 }
 
-function deltaTime(){
+function deltaTime() {
 	let now = Date.now();
 	dt = now - lastUpdate;
 	lastUpdate = now;
 }
 
-function getBestOfGeneration(){
-	let index = 0, best = 0;
-	for(let i = 0; i < invaders.population.length; i++){
-		if( invaders.population[i].fit > best ){
+function getBestOfGeneration() {
+    let index = 0, best = 0;
+    
+	for (let i = 0; i < invaders.population.length; i++) {
+		if (invaders.population[i].fit > best) {
 			best = invaders.population[i].fit;
 			index = i;
 		}
-	}
-	if( !invaders.bestOfGeneration || invaders.population[index].fit > invaders.bestOfGeneration.fit ){
+    }
+    
+	if (!invaders.bestOfGeneration || invaders.population[index].fit > invaders.bestOfGeneration.fit) {
 		invaders.bestOfGeneration = invaders.population[index];
 	}
 }
 
-function gameOver(){
+function gameOver() {
 	c.fillStyle = "white";
-	c.fillRect(0,0,w,h);
+	c.fillRect(0, 0, w, h);
 	c.fillStyle = "black";
 	c.font = "10px Arial";
-	c.fillText("Generation: "+generation, 5, 10);
-	c.fillText("Invaders: "+lives, 5, 20);
+	c.fillText("Generation: " + generation, 5, 10);
+	c.fillText("Invaders: " + lives, 5, 20);
 	let txt = "Game Over!";
 	c.font = "30px Arial";
-	c.fillText(txt, (w-c.measureText(txt).width)/2, h/2);
+	c.fillText(txt, (w - c.measureText(txt).width) / 2, h / 2);
 }
 
-function update(){
+function update() {
 	c.fillStyle = "white";
-	c.fillRect(0,0,w,h);
+	c.fillRect(0, 0, w, h);
 	c.fillStyle = "black";
 	c.font = "10px Arial";
-	c.fillText("Generation: "+generation, 5, 10);
-	c.fillText("Invaders: "+lives, 5, 20);
-	for(let i = 0; i < invaders.population.length; i++){
+	c.fillText("Generation: " + generation, 5, 10);
+	c.fillText("Invaders: " + lives, 5, 20);
+    
+    for (let i = 0; i < invaders.population.length; i++) {
 		invaders.population[i].show();
-	}
+    }
+    
 	player.show();
 	let allDead = true;
-	for(let i = 0; i < invaders.population.length; i++){
-		if( invaders.population[i].isAlive ){
+    
+    for (let i = 0; i < invaders.population.length; i++) {
+		if (invaders.population[i].isAlive) {
 			allDead = false;
 			break;
 		}
-	}
-	if(allDead){
+    }
+    
+	if (allDead) {
 		getBestOfGeneration();
-		if(generation%7){
+		if (generation % 7) {
 			invaders.evolve();
-		}else{
+		} else {
 			invaders.elitism();
 		}
 		generation++;
-	}
-	if(lives > 4){
+    }
+    
+	if (lives > 4) {
 		gameOver();
 		return;
 	}
-	deltaTime();
+    
+    deltaTime();
 	requestAnimationFrame(update);
 }
 
-function addEvents(){
-	document.addEventListener("keydown",function(e){
-		switch(e.keyCode){
-			case 13 :
-					init();
+function addEvents() {
+	document.addEventListener("keydown", function (e) {
+		switch (e.keyCode) {
+			case 13:
+				init();
 				break;
-			case 32 :
-					player.shoot();
+			case 32:
+				player.shoot();
 				break;
-			case 37 :
-			case 65 :
-					player.isMovingLeft = true;
+			case 37:
+			case 65:
+				player.isMovingLeft = true;
 				break;
-			case 39 :
-			case 68 :
-					player.isMovingRight = true;
-				break;
-		}
-	});
-
-	document.addEventListener("keyup",function(e){
-		switch(e.keyCode){
-			case 37 :
-			case 65 :
-					player.isMovingLeft = false;
-				break;
-			case 39 :
-			case 68 :
-					player.isMovingRight = false;
+			case 39:
+			case 68:
+				player.isMovingRight = true;
 				break;
 		}
 	});
 
-	window.addEventListener("focus",function(){
+	document.addEventListener("keyup", function (e) {
+		switch (e.keyCode) {
+			case 37:
+			case 65:
+				player.isMovingLeft = false;
+				break;
+			case 39:
+			case 68:
+				player.isMovingRight = false;
+				break;
+		}
+	});
+
+	window.addEventListener("focus", function () {
 		lastUpdate = Date.now();
 	});
 
-	fireBtn.addEventListener('touchstart', function(){
-		if( lives > 4){
+	fireBtn.addEventListener('touchstart', function () {
+		if (lives > 4) {
 			init();
-		}else{
-			player.shoot();
-		}
-	});
-	
-	fireBtn.addEventListener('mousedown', function(){
-		if( lives > 4){
-			init();
-		}else{
+		} else {
 			player.shoot();
 		}
 	});
 
-	leftBtn.addEventListener('touchstart', function(){
+	fireBtn.addEventListener('mousedown', function () {
+		if (lives > 4) {
+			init();
+		} else {
+			player.shoot();
+		}
+	});
+
+	leftBtn.addEventListener('touchstart', function () {
 		player.isMovingLeft = true;
 	});
 
-	leftBtn.addEventListener('touchend', function(){
+	leftBtn.addEventListener('touchend', function () {
 		player.isMovingLeft = false;
 	});
-	
-	leftBtn.addEventListener('mousedown', function(){
+
+	leftBtn.addEventListener('mousedown', function () {
 		player.isMovingLeft = true;
 	});
 
-	leftBtn.addEventListener('mouseup', function(){
+	leftBtn.addEventListener('mouseup', function () {
 		player.isMovingLeft = false;
-	});	
+	});
 
-	rightBtn.addEventListener('touchstart', function(){
+	rightBtn.addEventListener('touchstart', function () {
 		player.isMovingRight = true;
 	});
 
-	rightBtn.addEventListener('touchend', function(){
+	rightBtn.addEventListener('touchend', function () {
 		player.isMovingRight = false;
 	});
-	
-	rightBtn.addEventListener('mousedown', function(){
+
+	rightBtn.addEventListener('mousedown', function () {
 		player.isMovingRight = true;
 	});
 
-	rightBtn.addEventListener('mouseup', function(){
+	rightBtn.addEventListener('mouseup', function () {
 		player.isMovingRight = false;
 	});
-	
-	window.addEventListener('load', function(e) {
 
-		window.applicationCache.addEventListener('updateready', function(e) {
-		  if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+	window.addEventListener('load', function (e) {
+		window.applicationCache.addEventListener('updateready', function (e) {
+			if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
 				window.applicationCache.swapCache();
 				window.location.reload();
-		  }
+			}
 		}, false);
-
 	}, false);
 
 
-	if('serviceWorker' in navigator) {
+	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker
-		         .register('/invaderz/sw.js')
-		         .then(function() { });
+			.register('/invaderz/sw.js')
+			.then(function () { });
 	}
 
 	let deferredPrompt;
@@ -225,14 +231,14 @@ function addEvents(){
 		e.preventDefault();
 		deferredPrompt = e;
 		addBtn.addEventListener('click', (e) => {
-		  addBtn.style.display = 'none';
-		  deferredPrompt.prompt();
-		  deferredPrompt.userChoice.then((choiceResult) => {
-		      deferredPrompt = null;
-		    });
+			addBtn.style.display = 'none';
+			deferredPrompt.prompt();
+			deferredPrompt.userChoice.then((choiceResult) => {
+				deferredPrompt = null;
+			});
 		});
 	});
-	
+
 }
 
 addEvents();
